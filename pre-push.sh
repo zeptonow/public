2cd4d421ec95d05c326f5e12067d035b841d2346
zepto-gaurav <158581950+zepto-gaurav@users.noreply.github.com>
2025-05-14 16:17:20 +0530

    Update pre-push.sh

--- pre-push.sh
+++ pre-push.sh
@@ -1,21 +1,16 @@
 #!/bin/bash
-
 # Get the current remote and URL
 remote=$(git remote)
 url=$(git remote get-url "$remote")
-
-
 # Log file and remote logging URL
 LOG_FILE="$HOME/.git/hooks/push_attempts.log"
 REMOTE_LOGGING_URL="https://viy7077zbe.execute-api.ap-south-1.amazonaws.com/prod/log"
-
 log_and_notify() {
     local timestamp=$(date +"%Y-%m-%d %H:%M:%S")
     local hostname=$(hostname)
     local username=$(whoami)
     local remote=$(git remote)
     local url=$(git remote get-url "$remote")
-
     local payload=$(jq -n \
         --arg timestamp "$timestamp" \
         --arg hostname "$hostname" \
@@ -24,17 +19,11 @@ log_and_notify() {
         --arg git_remote "$remote" \
         '{timestamp: $timestamp, hostname: $hostname, username: $username, repository: $repository, git_remote: $git_remote}'
     )
-
     echo "$timestamp - BLOCKED: User $username on $hostname attempted to push to non-zeptonow repository: $url" >> "$LOG_FILE"
     echo "$payload" > "$HOME/.git/hooks/last_payload.json"
-
     logger -p auth.warning "Git Push Security Alert: User $username on $hostname tried to push to non-zeptonow repository: $url"
-
-    (curl -s -X POST -H "Content-Type: application/json" -d "$payload" "$REMOTE_LOGGING_URL" &) 2>/dev/null
+    curl -s -X POST -H "Content-Type: application/json" -d "$payload" "$REMOTE_LOGGING_URL" > /dev/null 2>&1 &
 }
-
-
-
 # Check if pushing to any non-zeptonow repository
 # This specifically checks for "zeptonow" in the URL (not just "zepto")
 if [[ $url != *"zeptonow"* ]]; then
@@ -50,5 +39,4 @@ if [[ $url != *"zeptonow"* ]]; then
     
     exit 1
 fi
-
 exit 0
